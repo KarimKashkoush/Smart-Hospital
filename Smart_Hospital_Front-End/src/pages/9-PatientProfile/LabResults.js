ffdb36c29b1b1f85fe5fbf670b89108c556f134b
KarimKashkoush <karimkashkoush5@gmail.com>
2025-05-26 21:27:20 +0300

    add

--- Smart_Hospital_Front-End/src/pages/9-PatientProfile/LabResults.js
+++ Smart_Hospital_Front-End/src/pages/9-PatientProfile/LabResults.js
@@ -11,7 +11,7 @@ const LabResults = () => {
     const fetchLabTests = async () => {
       try {
 
-        const response = await fetch(`http://localhost:5987/getPatient/${userId}`);
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/getPatient/${userId}`);
         const data = await response.json();
         setLabResults(data.LabTest || []);
 
